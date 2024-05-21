021f4082dbfd8de54e1dfa454da6829d79dfdf9e
Yaroslav <verbovyar@gmail.com>
2024-05-21 03:32:20 +0300

    modif

--- scripts/ddl_script.sql
+++ scripts/ddl_script.sql
@@ -1,23 +1,26 @@
 CREATE SCHEMA IF NOT EXISTS fs;
 
+CREATE TYPE ganre_enum AS ENUM('драма', 'фэнтези', 'криминал', 'биография', 'история'
+    'военный', 'фантастика', 'триллер', 'детектив', 'боевик', 'приключения', 'криминал');
+
 CREATE TABLE IF NOT EXISTS fs.genre(
-    genre           VARCHAR(20)             PRIMARY KEY,
-    accept_age      INTEGER                 NOT NULL
+    genre           ganre_enum              PRIMARY KEY,
+    accept_age      INTEGER                 NOT NULL CHECK(accept_age >= 1)
 );
 
 CREATE TABLE IF NOT EXISTS fs.films(
     film_id         INTEGER                 PRIMARY KEY,
-    film_title      CHARACTER VARYING(50)   NOT NULL,
+    film_title      CHARACTER VARYING(50)   NOT NULL CHECK(film_title != ''),
     rel_date        DATE                    NOT NULL,
     rel_country     CHARACTER VARYING(20)   NOT NULL,
-    genre           VARCHAR(20)             REFERENCES fs.genre(genre),
-    box_office      INTEGER                 NOT NULL,
+    genre           ganre_enum              REFERENCES fs.genre(genre),
+    box_office      INTEGER                 NOT NULL CHECK(box_office >= 0),
     director        CHARACTER VARYING(20)   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS fs.halls(
     hall_name       CHARACTER VARYING(20)   PRIMARY KEY,
-    seats_numb      INTEGER                 NOT NULL,
+    seats_numb      INTEGER                 NOT NULL CHECK(seats_numb >= 1),
     audio_format    CHARACTER VARYING(20)   NOT NULL
 );
 
@@ -30,18 +33,18 @@ CREATE TABLE IF NOT EXISTS fs.sessions(
 
 CREATE TABLE IF NOT EXISTS fs.spectators(
     spect_id        INTEGER                 PRIMARY KEY,
-    firstname       CHARACTER VARYING(20)   NOT NULL,
-    surname         CHARACTER VARYING(20)   NOT NULL,
-    sex             VARCHAR(10)             NOT NULL,
-    age             INTEGER                 NOT NULL,
-    comp_size       INTEGER                 NOT NULL
+    firstname       CHARACTER VARYING(20)   NOT NULL CHECK(firstname != ''),
+    surname         CHARACTER VARYING(20)   NOT NULL CHECK(surname != ''),
+    sex             ENUM('male', 'female')  NOT NULL,
+    age             INTEGER                 NOT NULL CHECK(age >= 1),
+    comp_size       INTEGER                 NOT NULL CHECK(comp_size >= 1)
 );
 
 CREATE TABLE IF NOT EXISTS fs.seats(
     seat_id         INTEGER                 PRIMARY KEY,
     hall_name       CHARACTER VARYING(20)   REFERENCES fs.halls(hall_name),
-    row             INTEGER                 NOT NULL,
-    seat            INTEGER                 NOT NULL
+    row             INTEGER                 NOT NULL CHECK(row >= 1),
+    seat            INTEGER                 NOT NULL CHECK(seat >= 1)
 );
 
 CREATE TABLE IF NOT EXISTS fs.tickets(
@@ -50,13 +53,15 @@ CREATE TABLE IF NOT EXISTS fs.tickets(
     sess_id         INTEGER                 REFERENCES fs.sessions(sess_id),
     seat_id         INTEGER                 REFERENCES fs.seats(seat_id),
     start_time      TIMESTAMP               NOT NULL,
-    cost            INTEGER                 NOT NULL
+    cost            INTEGER                 NOT NULL CHECK(cost >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS fs.history(
-    sess_id         INTEGER                 PRIMARY KEY,
+    sess_id         INTEGER                 NOT NULL REFERENCES fs.sessions(sess_id),
     film_id         INTEGER                 REFERENCES fs.films(film_id),
     hall_name       CHARACTER VARYING(20)   REFERENCES fs.halls(hall_name),
     start_time      TIMESTAMP               NOT NULL,
-    history         TIMESTAMP               NOT NULL
+    modif_time      TIMESTAMP               NOT NULL,
+
+    PRIMARY KEY (sess_id, modif_time)
 );
